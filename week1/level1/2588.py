04cf10abcbfd95f2b970a06cf95cb25df6bdf350
chungchung234 <a12812@kyonggi.ac.kr>
2022-01-04 05:53:24 +0900

    .

---
+++ week1/level1/2588.py
@@ -0,0 +1,22 @@
+"""
+===============================
+문제출처  : 백준
+문제번호  : 2588
+문제레벨  : level1
+학습주차  : week1
+최초생성  : 2022.01.04
+생성자    : chung
+문제설명  : multi
+===============================
+"""
+import sys
+
+A = int(sys.stdin.readline())
+B = str(sys.stdin.readline())
+
+print(A*int(B[2]))
+print(A*int(B[1]))
+print(A*int(B[0]))
+print(A*int(B))
+
+
