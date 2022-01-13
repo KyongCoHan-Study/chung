d264eaf7aed849d1e0a88217fcd13e4e349dd58c
chungchung234 <a12812@kyonggi.ac.kr>
2022-01-13 12:06:40 +0900

    .

--- 백준/level_12/10814.py
+++ 백준/level_12/10814.py
@@ -1,19 +1,17 @@
 """
 ===============================
 문제출처  : 백준
-문제번호  : 11650
+문제번호  : 10814
 문제단계  : level_12
-최초생성  : 2022.01.12
+최초생성  : 2022.01.13
 생성자    : chung
-문제설명  : sort
+문제설명  : 정렬
 ===============================
 """
 
 import sys
 
 N = int(sys.stdin.readline())
-xy=[list(map(int,sys.stdin.readline().split()))for _ in range(N)]
-xy.sort(key=lambda x:x[0])
-xy.sort(key=lambda x:x[1])
-
+xy=[list(sys.stdin.readline().split())for _ in range(N)]
+xy.sort(key=lambda x:inti(x[0]))
 [print(x[0],x[1]) for x in xy]
