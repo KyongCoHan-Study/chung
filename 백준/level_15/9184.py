a472f4fab9e6db015f6f629c4eab6f1a150bc151
chungchung234 <a12812@kyonggi.ac.kr>
2022-01-21 09:11:09 +0900

    .

--- 백준/level_15/9184.py
+++ 백준/level_15/9184.py
@@ -9,19 +9,24 @@
 ===============================
 """
 import sys
-while True:
-    a, b, c= map(int, sys.stdin.readline().split())
-    if a==-1 and b==-1 and c==-1: break
-    
-    
-    
-
 def w(a,b,c):
     if a<=0 or b<=0 or c<=0:
         return 1
     if a>20 or b>20 or c>20:
-        return w(20,20,20)
-    if a<b and b<c:
-        return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
-    w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        w(20,20,20)
+    if wn[a][b][c]:
+        return wn[a][b][c]
+    if a<b<c:
+        wn[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return wn[a][b][c]
+    wn[a][b][c]=w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return wn[a][b][c]
+wn=[[[0]*21 for _ in range(21)]for _ in range(21)]
+while True:
+    a, b, c= map(int, sys.stdin.readline().split())
+    if a==-1 and b==-1 and c==-1: break
+    w(a,b,c)
+    print("w({0}, {1}, {2}) = {3}".format(a,b,c,w(a,b,c)))
     
+    
+
