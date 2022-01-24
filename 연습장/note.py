de786667093d09671f4afb644767383769b5f19b
chungchung234 <a12812@kyonggi.ac.kr>
2022-01-24 20:06:13 +0900

    Update note.py

--- 연습장/note.py
+++ 연습장/note.py
@@ -1,29 +1,17 @@
-"""
-===============================
-문제출처  : chung
-문제번호  : note
-문제단계  : 연습장
-최초생성  : 2022.01.23
-생성자    : chung
-문제설명  : asdasd
-===============================
-"""
-def solution(answers):
-    answer = []
-    ans_1=[1, 2, 3, 4, 5]*2001
-    ans_2=[2, 1, 2, 3, 2, 4, 2, 5]*126
-    ans_3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]*1001
-    cnt=[0,0,0]
-    for i, ans in enumerate(answers):
-        if ans_1[i]==ans:
-            cnt[0]+=1
-        if ans_2[i]==ans:
-            cnt[1]+=1
-        if ans_3[i]==ans:
-            cnt[2]+=1
-    for i,j in enumerate(cnt):
-        if j == max(cnt):
-            answer.append(i+1)
-    return answer
+N = int(input())
+one=[0]*41
+zero=[0]*41
 
-print(solution([1,2,3,4,5]*2000))
+one[1]=1
+zero[0]=1
+for _ in range(N):
+    nums=(int(input()))
+    
+    if nums>2:
+        for i in range(2,nums+1):
+            if one[i]==0:
+                one[i]=one[i-2]+one[i-1]
+                zero[i]=zero[i-2]+zero[i-1]
+    print(zero[nums],one[nums])
+        
+        
