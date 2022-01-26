9be4bce070dae6ef736cb1607e024ec45922e810
chungchung234 <a12812@kyonggi.ac.kr>
2022-01-26 09:51:38 +0900

    .

--- 연습장/note.py
+++ 연습장/note.py
@@ -1,17 +1,37 @@
-N = int(input())
-one=[0]*41
-zero=[0]*41
 
-one[1]=1
-zero[0]=1
-for _ in range(N):
-    nums=(int(input()))
     
-    if nums>2:
-        for i in range(2,nums+1):
-            if one[i]==0:
-                one[i]=one[i-2]+one[i-1]
-                zero[i]=zero[i-2]+zero[i-1]
-    print(zero[nums],one[nums])
+def solution(name):
+
+    
+    
+    answer = 0
+    new_name=[]
+    alpha=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    loc=0
+    spell=0
+    for n in name:
+        for i,e in enumerate(alpha):
+            if n==e:
+                new_name.append(i)
+    while True:
+        if sum(new_name)==100*len(new_name):
+            break
+        minn=100
+        tspell=spell
+        tloc = loc
+        for i,n in enumerate(new_name):
+            if n == 100:
+                continue
+            if minn > abs(min(loc-i,i-len(new_name)+loc)+min(abs(n),abs(24-n))):
+                        minn = abs(min(abs(loc-i),abs(i-len(new_name)+loc)))+min(abs(n),abs(24-n))
+                        tloc=i
+                        tspell=n
+        answer+=minn
+        print(answer)
+        loc=tloc
+        spell=tspell
+        new_name[loc]=100
         
-        
+    return answer
+JAAAAAJAAAAAAAAAAAAJ 
+print(solution("JEROEN"))
