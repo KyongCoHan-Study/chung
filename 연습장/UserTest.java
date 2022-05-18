ce2c2cb2c3887c79a3d189941ffa66a72d013756
이충현 <a8207637@naver.com>
2022-05-18 14:44:19 +0900

    Update UserTest.java

--- 연습장/UserTest.java
+++ 연습장/UserTest.java
@@ -1,22 +1,44 @@
+import java.io.IOException;
+
 public class UserTest {
-	
+	static class IllegalParamException extends RuntimeException {
+		public IllegalParamException(String msg) {
+			super(msg);
+		}
+	}
+	static class Person {
+		private String name;
+		private int age;
+
+		public Person(String name, int age) throws IOException {
+			if (age < 0)
+				throw new IllegalParamException("Invalid input age : " + age);
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+	}
+
+
+
 	public static void main(String[] argv) {
-
-		String stringObj = "Hello";
-		int intValue = 0;
-		Float floatObj = new Float(1.0);
-
-		setValues(stringObj, intValue, floatObj);
-
-		System.out.println( stringObj + ", "
-				+ intValue + ", " + floatObj );
-
+		new UserTest().test();
 	}
 
-	public static void setValues(String strValue, int intValue, Float floatObj) {
-		strValue.replace("H", "h");
-		strValue += " World";
-		intValue = 99;
-		floatObj.valueOf((float) 2.0);
+	private void test() {
+		try {
+			Person person = new Person("John", -10);
+		} catch (IOException e) {
+			System.out.println("Exception catched");
+		} finally {
+			System.out.println("execute finally block");
+		}
 	}
+
+
 }
